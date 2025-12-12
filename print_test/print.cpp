31a4dea6608f132573a7791b648c9822a49c06d4
lim123123 <f499314e@gmail.com>
2025-12-12 14:54:42 +0900

    print test

---
+++ print_test/print.cpp
@@ -0,0 +1,7 @@
+#include <iostream>   // 입출력 기능 사용
+using namespace std;  // std:: 생략 가능하게 함
+
+int main() {
+    cout << "Hello, C++!" << endl; // 화면에 출력
+    return 0;                      // 프로그램 정상 종료
+}
